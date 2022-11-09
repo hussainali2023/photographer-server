0df88cfd937f8a13d796a66efa1ae9c6dec0861a
hussain2023 <iamhussainali2@gmail.com>
2022-11-09 20:55:18 +0530

    successfully send the data in server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -24,6 +24,7 @@ const client = new MongoClient(uri, {
 const run = async () => {
   try {
     const servicesCollection = client.db("photographer").collection("services");
+    const reviewsCollection = client.db("photographer").collection("reviews");
 
     app.get("/services-home", async (req, res) => {
       const query = {};
@@ -39,6 +40,20 @@ const run = async () => {
       const cursor = servicesCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.send(service);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const reviews = req.body;
+      console.log(reviews);
+      const result = await reviewsCollection.insertOne(reviews);
+      res.send(result);
     });
   } catch {
     console.error(error);
